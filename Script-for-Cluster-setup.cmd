a2e8b95b11fc11587ac888f2536a0ece260d8442
farha-sayed <91874664+farha-sayed@users.noreply.github.com>
2024-05-12 22:31:43 +0400

    Update Script-for-Cluster-setup.cmd

--- Script-for-Cluster-setup.cmd
+++ Script-for-Cluster-setup.cmd
@@ -1,11 +1,26 @@
 REM The instructions for the 4 cluster setups are outlined below.
 
 
-REM 1. Deploying TeaStore
+REM 1. Deploying TeaStore in every cluster
 
-REM We create and save the TeaStore file.
+REM We create and save the TeaStore YAML file.
 nano teastore-clusterip.yaml
 
-REM 
+REM We deploy the TeaStore file.
+kubectl create -f teastore-clusterip.yaml
+
+
+REM 2. Deploying HPA Object in every cluster
+
+REM We create and save the HPA YAML file
+nano hpa.yaml
+
+REM We deploy the HPA object
+kubectl create -f hpa.yaml
+
+
+REM 3. Set up Prometheus by following the instructions from Prometheus-and-Grafana-Setup.cmd
+
+
 
 
